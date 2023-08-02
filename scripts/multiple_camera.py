75d6b239125a2745a2f25c76e3f197342e9751fb
taeyeonggim <downoutnah@gmail.com>
2023-08-02 12:09:21 +0900

    camera_manager with service and service call

--- scripts/multiple_camera.py
+++ scripts/multiple_camera.py
@@ -1,117 +1,274 @@
 #!/usr/bin/env python3.7
-import argparse
 import rospy
 import subprocess
+import argparse
 
-from sensor_msgs.msg import Image
+
+from plaif_msgs.srv import CameraCapture, CameraCaptureResponse
+from plaif_msgs.msg import CameraData
 from ros_logger import ROSLogger
-from multiple_realsense.srv import LaunchState, LaunchStateResponse
+from sensor_msgs.msg import PointCloud2, Image
+from sensor_msgs.msg import CameraInfo
+from std_srvs.srv import Empty, EmptyResponse, EmptyRequest,SetBool, SetBoolResponse, SetBoolRequest
+import time
+
+
+class Camera:
+    def __init__(
+        self,
+        logger:ROSLogger,
+        lauch_file_name:str = 'rs_rgbd_l515.launch',
+        cam_name:str = 'camera01',
+        cam_switch_service_name:str = ''
+
+        ) -> None:
+        self.logger = logger
+
+        self.cam_name = cam_name
+        self.launch_file_name = lauch_file_name
+
+        self.camera_info_topic_name = f'/{cam_name}/color/camera_info'
+        self.camera_info:CameraInfo = None
+
+        self.color_topic_name = f'/{cam_name}/color/image_raw'
+        self.points_topic_name = f'/{cam_name}/depth_registered/points'
+
+        self.enable_service_name = f'/{cam_name}/enable'
+        self.reset_service_name = f'/{cam_name}/realsense2_camera/reset'
+
+        self.realsense_enable_service_call:rospy.ServiceProxy = None
+        self.realsense_reset_service_call:rospy.ServiceProxy = None
+
+        self.state = False
+        self.launch_process:subprocess.Popen = None
+
+        self.color_data:Image = None
+        self.point_data:PointCloud2 = None
+
+        rospy.Service(
+            f"/camera_manager/{cam_name}/capture", CameraCapture, self.get_cam_data
+        )
+
+        self.camera_manager_switch = rospy.ServiceProxy(cam_switch_service_name, Empty)
+
+    def launch_run(self):
+        """Run lauch file
+
+        Initialize the enable and reset service calls
+        """
+        self.logger.info(f'{self.cam_name} turning on... Please reboot if more than 5 minutes')
+        self.launch_process = subprocess.Popen(
+            ["roslaunch", "realsense2_camera", self.launch_file_name],
+            stdout=subprocess.DEVNULL
+        )
+
+        try:
+            self.camera_info = rospy.wait_for_message(self.camera_info_topic_name, CameraInfo, timeout=30)
+        except:
+            self.reset()
+    
+                
+        self.realsense_enable_service_call = rospy.ServiceProxy(self.enable_service_name, SetBool)
+        self.realsense_reset_service_call = rospy.ServiceProxy(self.reset_service_name, Empty)
+
+        self.state = True
+        self.logger.info(f'{self.cam_name} turned on')
+
+    def enable(self, req):
+        """
+        """
+        success = False
+        
+        if req:
+            if self.state ==False:
+                response = self.realsense_enable_service_call(True)
+                success = response.success
+                self.state = True
+
+        else:
+            if self.state == True:
+                response = self.realsense_enable_service_call(False)
+                success = response.success
+                self.state = False
+        
+        if not success:
+            success = self.reset()
+
+        return success
+    
+    def get_cam_data(self, req):
+        """ get the color and points data from camera
+        
+        This is under the condition that self.state is True
+        """
+        response = CameraCaptureResponse()
+        cam_data = CameraData()
+        
+        success = False        
+        
+        try:
+            if not self.state:
+                self.camera_manager_switch(EmptyRequest())
+            
+            self.color_data = rospy.wait_for_message(self.color_topic_name, Image, timeout=3.0)
+            self.point_data = rospy.wait_for_message(self.points_topic_name, PointCloud2, timeout=3.0)
+            success = True
+            
+        except:
+            success = self.reset(True)
+            
+        if success:
+            cam_data.color = self.color_data
+            cam_data.pointcloud = self.point_data
+            cam_data.camera_info = self.camera_info  
+                     
+            response.data = cam_data
+            
+        response.success = success
+    
+        return response
+
+    def reset(self, check_rgbd=False):
+        """reset the camera
+        
+        This resets the camera and try to get the needed data again
+        If the required data acquisition fails under reset, the camera process is stopped
+        """
+        self.logger.info(f'{self.cam_name} resetting!!!')
+        
+        success = True
+
+        try:
+            self.realsense_reset_service_call(EmptyRequest())
+            time.sleep(5)
+
+            rospy.wait_for_service(self.reset_service_name, timeout=60.0)            
+
+            self.camera_info = rospy.wait_for_message(self.camera_info_topic_name, CameraInfo, timeout=30.0)
+
+            if check_rgbd:
+                self.color_data = rospy.wait_for_message(self.color_topic_name, Image, timeout=3.0)
+                self.point_data = rospy.wait_for_message(self.points_topic_name, PointCloud2, timeout=3.0)\
+                    
+            self.state = True
+
+        except:
+            self.logger.err(f'{self.cam_name} not working. REBOOT PC!!!')
+            self.launch_process.terminate()
+            success = False
+            
+            for i in range(10):
+                self.logger.err(f'{self.cam_name} not working. REBOOT PC!!!')
+
+                time.sleep(0.5)
+
+        return success
 
 
 class CameraManager:
+
     def __init__(self, args, logger):
+
+
         self.args = args
         self.logger = logger
-        self.response = LaunchStateResponse()
-        self.avoid_camera_launch = None
-        self.camera01_launch = None
-        self.camera02_launch = None
+        
+        cam_switch_service_name = "/camera_manager/switch"
+        
+        rospy.Service(
+            '/camera_manager/kill_all', Empty, self.kill_all
+        )
+        
+        rospy.Service(
+            cam_switch_service_name, Empty, self.switch_camera
+        )
 
-    def avoid_camera_enable(self, req):
-        if req.enable:
-            avoid_camera_launch = subprocess.Popen(
-                ["roslaunch", "realsense2_camera", "rs_rgbd_d435i.launch"]
-            )
-            color_check = rospy.wait_for_message(
-                "/camera/color/image_raw", Image, timeout=None
-            )
-            if color_check:
-                self.response.success = True
-                self.logger.info("avoid_camera launched.")
-            else:
-                self.response.success = False
-                self.logger.info("avoid_camera launch failed.")
+        self.camera01 = Camera(self.logger,
+                               args.cam1_launch,
+                               'camera01',
+                               cam_switch_service_name
+                               )
+
+        self.camera02 = Camera(self.logger,
+                               args.cam2_launch,
+                               'camera02',
+                               cam_switch_service_name)
+
+        self.avoid_cam = Camera(self.logger,
+                               args.avoid_cam_launch,
+                               'camera')       
+        
+        self.init_launch()
+
+    def init_launch(self):
+        ## 카메라 A와 B를 각각 순차적으로 실행하는 코드 작성 ##
+
+        self.camera01.launch_run()
+        time.sleep(4)
+        self.camera02.launch_run()
+        time.sleep(4)
+        self.camera02.enable(False)
+        time.sleep(0.5)
+        self.avoid_cam.launch_run()
+
+    def switch_camera(self, req):
+        """
+        """
+        if self.camera01.state:
+            try:
+                self.camera01.enable(False)
+                time.sleep(0.5)
+                self.camera02.enable(True)
+            except:
+                self.camera02.reset()
         else:
-            avoid_camera_launch.terminate()
-            self.logger.info("avoid_camera stopped.")
-            self.response.success = True
-        return self.response
+            try:
+                self.camera02.enable(False)
+                time.sleep(0.5)
+                self.camera01.enable(True)
+            except:
+                self.camera01.reset()
 
-    def camera01_enable(self, req):
-        if req.enable:
-            self.camera01_launch = subprocess.Popen(
-                ["roslaunch", "realsense2_camera", "rs_rgbd_d415.launch"]
-            )
-            color_check = rospy.wait_for_message(
-                "/camera01/color/image_raw", Image, timeout=None
-            )
-            if color_check:
-                self.response.success = True
-                self.logger.info("camera01 launched.")
-            else:
-                self.response.success = False
-                self.logger.info("camera01 launch failed.")
-        else:
-            self.camera01_launch.terminate()
-            self.logger.info("camera01 stopped.")
-            self.response.success = True
-        return self.response
-
-    def camera02_enable(self, req):
-        if req.enable:
-            color_check = rospy.wait_for_message(
-                "/camera02/color/image_raw", Image, timeout=None
-            )
-            self.camera02_launch = subprocess.Popen(
-                ["roslaunch", "realsense2_camera", "rs_rgbd_l515.launch"]
-            )
-            if color_check:
-                self.response.success = True
-                self.logger.info("camera01 launched.")
-            else:
-                self.response.success = False
-                self.logger.info("camera01 launch failed.")
-        else:
-            self.camera02_launch.terminate()
-            self.logger.info("camera01 stopped.")
-            self.response.success = True
-        return self.response
-
+        return EmptyResponse()
+    
+    def kill_all(self, req):
+        """
+        """
+        self.camera01.launch_process.terminate()
+        self.camera02.launch_process.terminate()
+        self.avoid_cam.launch_process.terminate()
+        
+        return EmptyResponse()
+        
 
 def get_arguments():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="recive camera, model and process configurations"
+    )
+
     parser.add_argument(
-        "--avoid_cam", type=str, help="Path to Camera Avoid (exclude HOME directory)"
+        "--avoid_cam_launch", type=str, help="avoid cam launch file name", default='rs_rgbd_d435i.launch'
     )
     parser.add_argument(
-        "--cam1", type=str, help="Path to Camera 1 (exclude HOME directory)"
+        "--cam1_launch", type=str, help="cam1 launch file name", default="rs_rgbd_d415_cam01.launch"
     )
     parser.add_argument(
-        "--cam2", type=str, help="Path to Camera 2 (exclude HOME directory)"
+        "--cam2_launch", type=str, help="cam2 launch file name", default="rs_rgbd_d415_cam02.launch"
     )
-    args = parser.parse_args()
+
+    args, _ = parser.parse_known_args()
 
     return args
 
 
 def main():
+    rospy.init_node("vs_camera_manager")
     args = get_arguments()
     logger = ROSLogger()
 
-    camera_manager = CameraManager(args, logger)
-
-    rospy.init_node("vs_camera_manager")
+    camera_manager = CameraManager(args,logger)
     camera_manager.logger.info("vs_camera_manager node created.")
 
-    rospy.Service(
-        "/camera/launch/enable", LaunchState, camera_manager.avoid_camera_enable
-    )
-    rospy.Service(
-        "/camera01/launch/enable", LaunchState, camera_manager.camera01_enable
-    )
-    rospy.Service(
-        "/camera02/launch/enable", LaunchState, camera_manager.camera02_enable
-    )
     rospy.spin()
 
 
